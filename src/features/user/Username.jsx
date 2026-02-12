e393e979261ce4f6c90ee088245506313ce2e9d0
samrawit <samrawitwubshet4@gmail.com>
2026-02-12 10:42:38 -0300

    initial commit

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,14 @@
+import { useStore } from "react-redux";
+import { useSelector } from "react-redux";
+
+function Username() {
+  const username = useSelector((store) => store.user.username);
+
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
+}
+
+export default Username;
---
+++ src/ui/Button.jsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+
+function Button({ children, disabled, to, type, onClick }) {
+  const base =
+    "transtion-colors text-sm inline-block cursor-pointer rounded-full bg-yellow-400 font-semibold tracking-wide text-stone-800 uppercase duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed";
+
+  const styles = {
+    primary: base + " px-4 py-3 sm:px-6 sm:py-4",
+    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    round: base + " px-2.5 py-1 md:px-3.5 md:py-2 text-sm",
+    secondary:
+      "transtion-colors text-sm inline-block cursor-pointer rounded-full border-2 border-stone-300 bg-transparent font-semibold tracking-wide text-stone-400 uppercase duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 px-4 py-2.5 sm:px-6 sm:py-3.5 focus:ring focus:ring-stone-200 focus:ring-offset-2 focus:text-stone-800 focus:outline-none disabled:cursor-not-allowed",
+  };
+
+  if (to)
+    return (
+      <Link className={styles[type]} to={to}>
+        {children}
+      </Link>
+    );
+
+  if (onClick) {
+    return (
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+        {children}
+      </button>
+    );
+  }
+
+  return (
+    <button disabled={disabled} className={styles[type]}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
